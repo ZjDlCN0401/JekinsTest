0700d56a5465ba9b99885fcfa78c5ce9a89131d5
DESKTOP-HBDLKCU\sinkatec031 <zj_dalian@yeah.net>
2024-04-17 16:46:11 +0800

    112

---
+++ src/test/java/cn/zj/jeckinstest/GreetingControllerTest.java
@@ -0,0 +1,16 @@
+package cn.zj.jeckinstest;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class GreetingControllerTest {
+
+  @Test
+  void greeting() {
+    Assertions.assertThat(1).isEqualTo(1);
+  }
+}
