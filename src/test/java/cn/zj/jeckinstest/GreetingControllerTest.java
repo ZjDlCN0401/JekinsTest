f7e657f70841de6a217f1b2f174012abd2e14560
DESKTOP-HBDLKCU\sinkatec031 <zj_dalian@yeah.net>
2024-04-24 15:24:43 +0800

    1

--- src/test/java/cn/zj/jeckinstest/GreetingControllerTest.java
+++ src/test/java/cn/zj/jeckinstest/GreetingControllerTest.java
@@ -11,6 +11,16 @@ class GreetingControllerTest {
 
   @Test
   void greeting() {
+    Assertions.assertThat(2).isEqualTo(1);
+  }
+  
+  @Test
+  void greeting2() {
     Assertions.assertThat(1).isEqualTo(1);
+  }
+  
+  @Test
+  void greeting3() {
+    Assertions.assertThat(3).isEqualTo(1);
   }
 }
